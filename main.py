6c1d7abc38fd76b7f04b91cd74b1cf48605019c4
aiyin <aiyin@dnssectest.live>
2024-07-10 19:50:33 -0700

    add divi examples

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ def multi(a, b):
 def minu(a, b):
     return a - b
 
-def divi(a,b):
+
+def divi(a, b):
     return a / b
+
 
 if __name__ == '__main__':
     print("hello world")
     print(add(5, 7))
     print(multi(5, 7))
+    print(divi(999, 9))
     print("bye world")
     sys.exit()
