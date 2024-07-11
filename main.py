7de879077f94218f45489291dd1dd1371f3c977d
aiyin <aiyin@dnssectest.live>
2024-07-10 18:41:51 -0700

    add multi func

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ def add(a, b):
     return a + b
 
 
+def multi(a, b):
+    return a * b
+
+
 if __name__ == '__main__':
     print("hello world")
     print(add(5, 7))
+    print(multi(5, 7))
     print("bye world")
     sys.exit()
